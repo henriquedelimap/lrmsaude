e16b68f19547ab259d67b99433c3eb4ca306d84a
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-06-06 15:42:22 -0300

    segundo teste

--- src/pages/Planos/index.tsx
+++ src/pages/Planos/index.tsx
@@ -1,8 +1,9 @@
 import { Itens } from "@/components/Itens";
-
+import styles from './Planos.module.scss'
 export function Planos(){
     return (
         <div>
+                <h2 className={styles.titulo}>planos de saúde</h2>
             <Itens />
         </div>
     )
