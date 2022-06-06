67d585794076f186875a37438edaef43f5a48c70
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-06-05 22:17:49 -0300

    segundo teste

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,11 +1,15 @@
 import infos from '@/data/info.json'
 import styles from './Carousel.module.scss'
+import photo from '@/assets/foto/bradescoSaude.png'
+
 export function Carousel(){
     return(
         <div className={styles.carousel}>
             {
                 infos.map((info)=>(
+                    <>
                     <img src={info.img} className={styles.carousel__img} />
+                    </>
                 ))
             }
         </div>
