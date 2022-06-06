1209cb7bd966546a785e6e97417bd49e9ab911c4
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-06-06 14:56:23 -0300

    segundo teste

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,9 +1,18 @@
 import styles from './Carousel.module.scss'
 import foto1 from '@/assets/foto/bradescoSaude.png'
 import foto2 from '@/assets/foto/notredame.png'
+import foto3 from '@/assets/foto/premiumsaude.png'
+import foto4 from '@/assets/foto/rnsaude.png'
+import foto5 from '@/assets/foto/seguros unimed.png'
+import foto6 from '@/assets/foto/sf.png'
+import foto7 from '@/assets/foto/sulamerica.png'
+import foto8 from '@/assets/foto/unimed.png'
+import foto9 from '@/assets/foto/unimedUberlandia.png'
+import foto10 from '@/assets/foto/usi.png'
+import foto11 from '@/assets/foto/you.png'
 
 export function Carousel(){
-    const fotos = [foto1, foto2]
+    const fotos = [foto1, foto2, foto3, foto4, foto5, foto6, foto7, foto8, foto9, foto10, foto11]
     return(
         <div className={styles.carousel}>
             {
