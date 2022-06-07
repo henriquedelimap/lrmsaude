24fe3ffe1894187417a84de4f7be9c3c7a515636
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-06-07 18:58:52 -0300

    resolvendo 2022

--- src/pages/Plano/index.tsx
+++ src/pages/Plano/index.tsx
@@ -1,5 +1,5 @@
 import { WhatsApp } from "@/components/Buttons/whatsapp";
-import { Imagens } from "@/assets";
+import { Imagens } from "@/imagens.tsx";
 import { IImagem, Plano } from "types/IPlano";
 import styles from './Plano.module.scss'
 export function PlanoPage(plano: Plano){
