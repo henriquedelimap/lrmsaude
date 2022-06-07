08348796fcd42fc909311ad768323ba92d5d22ed
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-06-07 11:10:16 -0300

    resolvendo

--- src/components/Categorias/Categoria/Description/index.tsx
+++ src/components/Categorias/Categoria/Description/index.tsx
@@ -1,18 +1,35 @@
 import { ReactElement } from "react";
 import { Info } from "types/IPlano";
 import styles from "./Description.module.scss"
+import { motion } from 'framer-motion'
 
 export function Description(info: Info ){
     let listItens;
     return(
-        <div className={styles.description}>
-            <div className={styles.description__administradoras}>
-            {
+        // <div className={styles.description}>
+        //     <div className={styles.description__administradoras}>
+        //     {
+        //         listItens = info.administradora.map((item)=>(
+        //                 <span>{item}</span>
+        //                 ))  
+        //             }
+        //     </div> 
+        // </div>
+
+
+    <motion.div  className={styles.description}>
+            <motion.div 
+                className={styles.description__administradoras}
+                drag='x'
+                dragConstraints={{right: 0, left: -500}}>
+
+    
+                {
                 listItens = info.administradora.map((item)=>(
-                        <span>{item}</span>
+                        <motion.div className={styles.description__administradoras__itens}>{item}</motion.div>
                         ))  
-                    }
-            </div>
-        </div>
+                }
+        </motion.div>
+        </motion.div >
     )
 }
