55b9ed88a58dd5830065384fe1321d9364dbc4ef
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-06-10 17:19:24 -0300

    header

--- src/components/Menu/Responsivo/index.tsx
+++ src/components/Menu/Responsivo/index.tsx
@@ -1,12 +1,36 @@
 import classNames from 'classnames'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import { Menu } from '..'
 import styles from './Responsivo.module.scss'
 
 export function MenuResponsivo(){
     const [isExtended, setIsExtended] = useState(true)
+    const rotas = [
+        {
+            to: '/',
+            label: 'Início',
+            id: 1
+        },
+        {
+            to: '/planos',
+            label: 'Planos',
+            id: 2
+
+        },
+        {
+            to: '/sobre',
+            label: 'Sobre',
+            id: 3
+        },
+        {
+            to: '/blog',
+            label: 'Blog',
+            id: 4
+        }
+    ]
     return(
-        <>
+        <nav>
         <div 
             className={classNames({
                 [styles.menuResponvivo]: true,
@@ -14,9 +38,28 @@ export function MenuResponsivo(){
             })}
             onClick={()=>setIsExtended(!isExtended)}
         >
+
             <div className={styles.menuResponvivo__box}>
                 <div>
+                    <ul className={styles.menuResponvivo__box__lista}>
+                        {
+                            rotas.map((rota)=>(
+                                <li 
+                                    key={rota.id}
+                                    
+                                    className={styles.menuResponvivo__box__lista__itens}
+                                >
+                                    <Link 
+                                        to={rota.to}
+                                        className={styles.menuResponvivo__box__lista__itens__item}
 
+                                    >
+                                        {rota.label}
+                                    </Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
                 </div>
                 <div className={styles.icone}>
                     <span className={styles.icone__rect} />
@@ -25,6 +68,6 @@ export function MenuResponsivo(){
             </div>
             
         </div>
-        </>
+        </nav>
     )
 }
