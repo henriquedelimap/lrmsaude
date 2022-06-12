85a8bb814f71997d21af7fe354156ce04e5c6f7e
Henrique de Lima Pessoa <Henrique@MacBook-Pro-de-Felipe.local>
2022-06-12 17:35:25 -0300

    home

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -8,8 +8,12 @@ import { GiHealthPotion } from 'react-icons/gi';
 import { HiOutlineArrowCircleRight } from 'react-icons/hi';
 
 import { Voltar } from '@/components/Buttons/default';
+import { useState } from 'react';
+import classNames from 'classnames';
 
 export function Inicio(){
+    const [ativaItem, setAtivaItem] = useState(false)
+
     const text = {title: 'hello', to: '/'}
     return(
         <>
@@ -49,6 +53,25 @@ export function Inicio(){
                 <div className={styles.home__txt}>
                     <span className={styles.home__txt__subtitle}>compare e conheça</span>
                     <span className={styles.home__txt__title}>a solução perfeita para sua saúde</span>
+                </div>
+
+                <div className={styles.home__lista__itens}>
+                    <div className={styles.home__lista__itens__item}>
+                        <div className={styles.home__lista__itens__item__title}>
+                            <h4 className={styles.home__lista__itens__item__title__text}>plano de saúde individual ou coletivo por Adesão</h4>
+                            <span 
+                                className={styles.home__lista__itens__item__title__icone}
+                                onClick={()=>setAtivaItem(!ativaItem)}
+                            >
+                                <span className={styles.home__lista__itens__item__title__icone__rect} />
+                                <span className={styles.home__lista__itens__item__title__icone__rect} />
+                            </span>
+                        </div>
+                        <p className={classNames({
+                            [styles.home__lista__itens__item__description]: true,
+                            [styles[`home__lista__itens__item__description--ativa`]]: ativaItem,
+                        })}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis alias tempora qui quidem quam repellat ipsum distinctio adipisci architecto perferendis!</p>
+                    </div>
                 </div>
             </div>
             {/* <Comparativo /> */}
